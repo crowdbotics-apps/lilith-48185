299e97c51e867decea5906d60b59a8f57cf7471e
Crowdbotics <team@crowdbotics.com>
2024-05-30 18:09:57 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "lilith-48185",
+  url: "https://lilith-48185.botics.co",
+  api: "https://lilith-48185.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
